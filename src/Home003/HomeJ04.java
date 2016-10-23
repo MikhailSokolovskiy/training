8f2ca372d8de2160aa30a86a60971c5f87922947
Mikhail Sokolovskiy <mdkcrash@ukr.net>
2016-10-23 20:32:15 +0300

    Fix H003/04

--- src/Home003/HomeJ04.java
+++ src/Home003/HomeJ04.java
@@ -12,21 +12,24 @@ public class HomeJ04 {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         String[] str = reader.readLine().split(" ");
         int resultIndex = 0;
-        int q=0;
+        int q = 0;
         int[] quan = new int[str.length];
 
-        for (int a=0;a<str.length;a++) {
+        for (int a = 0; a < str.length; a++) {
             for (int i = 0; i < str[a].length(); i++) {
                 for (int j = 0; j < str[a].length(); j++) {
-                    if (i==j) continue;
-                    if(str[a].charAt(i)==str[a].charAt(j)){
-                        quan[a]+=1;
+                    if (i == j) continue;
+                    if (str[a].charAt(i) == str[a].charAt(j)) {
+                        quan[a] += 1;
+                    } else {
+                        quan[a] -= 1;
                     }
+
                 }
             }
-            if (quan[a]>q){
-                q=quan[a];
-                resultIndex=a;
+            if (quan[a] > q) {
+                q = quan[a];
+                resultIndex = a;
             }
         }
 
