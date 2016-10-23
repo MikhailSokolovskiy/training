5ee89004cfea39ab7636ecb5af32f80acb27366a
Mikhail Sokolovskiy <mdkcrash@ukr.net>
2016-10-23 19:52:53 +0300

    Ok H003/03

---
+++ src/Home003/HomeJ03.java
@@ -0,0 +1,35 @@
+package Home003;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * Created by m.sokolovskiy on 23.10.2016.
+ */
+public class HomeJ03 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String[] str = reader.readLine().split(" ");
+        int middle = 0;
+        String lesMiddle = "", moMiddle = "";
+
+        for (String st : str) {
+            middle += st.length();
+        }
+        middle /= str.length;
+
+        for (String number : str) {
+            if (number.length() < middle) {
+                lesMiddle += number + " - " + number.length() + "\n";
+            } else if (number.length() > middle){
+                moMiddle += number + " - " + number.length() + "\n";
+            }
+        }
+        System.out.printf("Average length of all figures: %s\n", middle);
+        System.out.printf("Number there is less average length:\n%s\n", lesMiddle);
+        System.out.printf("Number there is more average length:\n%s", moMiddle);
+
+    }
+
+}
