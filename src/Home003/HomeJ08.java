2cf0a1fd3ad0339278281e1a386fae59128e95ae
Mikhail Sokolovskiy <mdkcrash@ukr.net>
2016-10-23 22:48:16 +0300

    Fix H003/08

---
+++ src/Home003/HomeJ08.java
@@ -0,0 +1,26 @@
+package Home003;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import static java.lang.Math.pow;
+
+/**
+ * Created by m.sokolovskiy on 23.10.2016.
+ */
+public class HomeJ08 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int mat = Integer.parseInt(reader.readLine());
+        int i = 1;
+
+        do {
+            System.out.print(i + " ");
+            if (i % mat == 0) {
+                System.out.println();
+            }
+            i++;
+        } while (i <= pow(mat, 2));
+    }
+}
