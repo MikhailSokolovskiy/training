f572c1a7f706daeb824a9e806eaaff6d1708649f
Mikhail Sokolovskiy <mdkcrash@ukr.net>
2016-10-23 19:27:22 +0300

    Refactoring

---
+++ src/Home003/HomeJ02.java
@@ -0,0 +1,39 @@
+package Home003;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+/**
+ * Created by m.sokolovskiy on 21.10.2016.
+ */
+public class HomeJ02 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String[] str = reader.readLine().split(" ");
+        boolean sort;
+
+        do {
+            sort = false;
+            for (int i = 0; i < str.length - 1; i++) {
+                if (str[i].length() > str[i + 1].length()) {
+                    String temp = str[i];
+                    str[i] = str[i + 1];
+                    str[i + 1] = temp;
+                    sort = true;
+                }
+            }
+        } while (sort);
+
+        System.out.print("On increase: ");
+        for (String number : str) {
+            System.out.print(number + " ");
+        }
+
+        System.out.print("\nOn decrease: ");
+        for (int i = str.length - 1; i >= 0; i--) {
+            System.out.print(str[i] + " ");
+        }
+    }
+}
