30b3f3c7d3677c3837efba17844b71d6fdc37626
Mikhail Sokolovskiy <mdkcrash@ukr.net>
2016-10-24 09:52:21 +0300

    fix H002/06

--- src/Home002/HomeJ06.java
+++ src/Home002/HomeJ06.java
@@ -13,9 +13,6 @@ import java.util.Arrays;
 public class HomeJ06 {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        String res1 = "", res2 = "";
-        int sum = 0;
-        ArrayList<Integer> list = new ArrayList<>();
 
         String conInp = reader.readLine();
         String[] conArrayS = conInp.split(" ");
@@ -25,6 +22,16 @@ public class HomeJ06 {
             num[i] = Integer.parseInt(conArrayS[i]);
         }
 
+        evenUnEvenn(num);
+        devBy39(conArrayS);
+        devBy57(num, conArrayS);
+        uniqThree(conArrayS);
+        happyNum(num);
+        equalHalfSum(num);
+    }
+
+    public static void evenUnEvenn(int[] num) {
+        String res1 = "", res2 = "";
         for (int i : num) {
             if (i % 2 == 0) {
                 res1 += i + " ";
@@ -34,12 +41,15 @@ public class HomeJ06 {
         }
         System.out.printf("Even: %s\nUneven: %s\n\n", res1, res2);
 
-
         Arrays.sort(num);
         System.out.printf("Max: %s\nMin: %s\n\n", num[num.length - 1], num[0]);
+    }
 
-        res1 = "";
-        for (String s : conArrayS) {
+    public static void devBy39(String[] str) {
+        String res1 = "";
+        int sum = 0;
+
+        for (String s : str) {
             for (int i = 0; i < s.length(); i++) {
                 sum += Character.getNumericValue(s.charAt(i));
             }
@@ -49,17 +59,24 @@ public class HomeJ06 {
             sum = 0;
         }
         System.out.printf("Divisible by 3 or 9: %s\n\n", res1);
+    }
 
-        res1 = "";
+    public static void devBy57(int[] num, String[] str) {
+        String res1 = "";
         for (int i : num) {
             if (i % 5 == 0 && i % 7 == 0) {
                 res1 += i + " ";
             }
         }
         System.out.printf("Divisible by 5 and 7: %s\n\n", res1);
+    }
 
-        res1 = "";
-        for (String s : conArrayS) {
+    public static void uniqThree(String[] str) {
+        String res1 = "";
+
+        for (String s : str)
+
+        {
             if (Integer.parseInt(s) >= 100 && Integer.parseInt(s) <= 999) {
                 if (s.charAt(0) != s.charAt(1) && s.charAt(0) != s.charAt(2) && s.charAt(1) != s.charAt(2)) {
                     res1 += s + " ";
@@ -67,8 +84,12 @@ public class HomeJ06 {
             }
         }
         System.out.printf("Unique three signs: %s\n\n", res1);
+    }
 
-        res1 = "";
+    public static void happyNum(int[] num) {
+        String res1 = "";
+        ArrayList<Integer> list = new ArrayList<>();
+
         for (int i = 1; i <= num[num.length - 1]; i++) {
             if (i % 2 != 0)
                 list.add(i);
@@ -92,8 +113,10 @@ public class HomeJ06 {
             }
         }
         System.out.printf("Happy numbers: %s\n\n", res1);
+    }
 
-        res1 = "";
+    public static void equalHalfSum(int[] num) {
+        String res1 = "";
         for (int i = 1; i < num[num.length - 2]; i++) {
             if (num[i] == (num[i - 1] + num[i + 1]) / 2) {
                 res1 += num[i] + " ";
