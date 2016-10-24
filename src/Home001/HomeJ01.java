4aef300e6078e97f7ab576a1fa39964d3f9a56db
Mikhail Sokolovskiy <mdkcrash@ukr.net>
2016-10-24 09:14:08 +0300

    fix H001/01

--- src/Home001/HomeJ01.java
+++ src/Home001/HomeJ01.java
@@ -7,18 +7,10 @@ import java.util.stream.IntStream;
  */
 public class HomeJ01 {
     public static void main(String[] args) {
-//        IntStream.rangeClosed('A', 'Z')
-//                .mapToObj(c -> "" + (char) c)
-//                .forEach(System.out::println);
-
         char a = 'A';
         do {
-            System.out.print((char) a + "" + ((char) (a + 32)) + " ");
+            System.out.print(a + "" + ((char) (a + 32)) + " ");
             a++;
         } while (a <= 'Z');
-
-        IntStream.range(65, 123) //range for ASCII table
-                .filter(n -> n < 91 || n > 96) // filter for exclude not alphabetic letters
-                .forEach(n -> System.out.print((char)n + " ")); // print
-    }
+   }
 }
