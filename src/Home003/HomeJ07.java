9719b70f6a42fb5ade3a0f01b8a2cda4d736c38a
Mikhail Sokolovskiy <mdkcrash@ukr.net>
2016-10-30 19:05:28 +0200

    Ok 80% H003/09 (1,2,3,_,5,6,_,8) fix 7

--- src/Home003/HomeJ07.java
+++ src/Home003/HomeJ07.java
@@ -14,7 +14,7 @@ public class HomeJ07 {
 
         for (String st : str) {
             boolean diff = true;
-            if (st.length() == 1) break;
+            if (st.length() == 1) continue;
             for (int i = 0; i < st.length(); i++) {
                 for (int j = 0; j < st.length(); j++) {
                     if (i == j) continue;
